a882bef40eb786dd3baa201f7cbb6481d4b8f162
EL MOBO <anastasiia.inc@yandex.ru>
2025-08-30 23:05:03 +0300

    edit qeury request to JOIN

--- internal/repository/order_repo.go
+++ internal/repository/order_repo.go
@@ -15,41 +15,69 @@ func NewOrderRepo(dataBase *sqlx.DB) *OrderRepo {
 	return &OrderRepo{dataBase}
 }
 
+const queryFullOrder = `
+SELECT
+	ord.order_uid, ord.track_number, ord.entry, ord.locale,
+	ord.internal_signature, ord.customer_id, ord.delivery_service,
+	ord.shardkey, ord.sm_id, ord.date_created, ord.oof_shard,
+	del.name, del.phone, del.zip, del.city, del.address,
+	del.region, del.email,
+	pay.transaction, pay.request_id, pay.currency, pay.provider,
+	pay.amount, pay.payment_dt, pay.bank,
+	pay.delivery_cost, pay.goods_total, pay.custom_fee,
+	itm.chrt_id, itm.price, itm.rid, itm.name, itm.sale,
+	itm.size, itm.total_price, itm.nm_id, itm.brand, itm.status
+FROM orders ord
+LEFT JOIN delivery del ON del.order_uid = ord.order_uid
+LEFT JOIN payment pay ON pay.transaction = ord.order_uid
+LEFT JOIN items itm ON itm.order_uid = ord.order_uid
+WHERE ord.order_uid = $1
+ORDER BY itm.chrt_id
+`
+
 func (repo *OrderRepo) GetFullOrder(ctx context.Context, uid string) (*models.Order, error) {
-	order := models.Order{}
-	//var deliveryStruct models.Delivery
-	//var paymentStruct models.Payment
-	//var itemStruct models.Item
-
-	queryOrder := `SELECT order_uid, track_number, entry, locale, internal_signature,
-	customer_id, delivery_service, shardkey, sm_id, date_created, oof_shard
-	FROM orders WHERE order_uid = $1`
-
-	queryDelivery := `SELECT name, phone, zip, city, address, region, email
-	FROM delivery WHERE order_uid = $1`
-
-	queryPayment := `SELECT "transaction", request_id, currency, provider, amount, payment_dt,
-	bank, delivery_cost, goods_total, custom_fee FROM payment WHERE "transaction" = $1`
-
-	queryItems := `SELECT chrt_id, price, rid, name,
-	sale, "size", total_price, nm_id, brand, status
-	FROM items WHERE order_uid = $1`
-
-	if err := repo.dataBase.GetContext(ctx, &order, queryOrder, uid); err != nil {
+	rows, err := repo.dataBase.QueryxContext(ctx, queryFullOrder, uid)
+	if err != nil {
 		return nil, err
 	}
+	defer rows.Close()
 
-	if err := repo.dataBase.GetContext(ctx, &order.Delivery, queryDelivery, uid); err != nil {
-		return nil, err
-	}
+	var (
+		order models.Order
+		items []models.Item
+	)
+	for rows.Next() {
+		var (
+			ord models.Order
+			del models.Delivery
+			pay models.Payment
+			itm models.Item
+		)
 
-	if err := repo.dataBase.GetContext(ctx, &order.Payment, queryPayment, uid); err != nil {
-		return nil, err
-	}
+		if err := rows.Scan(
+			&ord.Order_uid, &ord.Track_number, &ord.Entry, &ord.Locale,
+			&ord.Internal_signature, &ord.Customer_id, &ord.Delivery_service,
+			&ord.Shardkey, &ord.Sm_id, &ord.Date_created, &ord.Oof_shard,
 
-	var items []models.Item
-	if err := repo.dataBase.SelectContext(ctx, &items, queryItems, uid); err != nil {
-		return nil, err
+			&del.Name, &del.Phone, &del.Zip, &del.City, &del.Address,
+			&del.Region, &del.Email,
+
+			&pay.Transaction, &pay.Request_id, &pay.Currency, &pay.Provider,
+			&pay.Amount, &pay.Payment_dt, &pay.Bank, &pay.Delivery_cost,
+			&pay.Goods_total, &pay.Custom_fee,
+
+			&itm.Chrt_id, &itm.Price, &itm.Rid, &itm.Name, &itm.Sale,
+			&itm.Size, &itm.Total_price, &itm.Nm_id, &itm.Brand, &itm.Status,
+		); err != nil {
+			return nil, err
+		}
+
+		if order.Order_uid == "" {
+			order = ord
+			order.Delivery = del
+			order.Payment = pay
+		}
+		items = append(items, itm)
 	}
 	order.Items = items
 	return &order, nil
